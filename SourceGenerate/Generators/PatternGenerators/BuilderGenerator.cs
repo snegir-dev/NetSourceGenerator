10899fbd5af9832e9858ffb3bb4ecf8c42849257
SnEGiR <steamsnegir@gmail.com>
2022-05-03 20:40:09 +0300

    add generator for builder pattern

---
+++ SourceGenerate/Generators/PatternGenerators/BuilderGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using SourceGenerate.Domain.Attributes;
+using SourceGenerate.Templates.Patterns;
+
+namespace SourceGenerate.Generators.PatternGenerators;
+
+[Generator]
+public class BuilderGenerator : IIncrementalGenerator
+{
+    private readonly GeneratorHandler _generatorHandler = new(typeof(BuilderAttribute));
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var types = context.SyntaxProvider
+            .CreateSyntaxProvider(_generatorHandler.IsExistAttribute, _generatorHandler.GetTypeSymbolOrNull)
+            .Where(p => p != null)
+            .Collect();
+
+        context.RegisterSourceOutput(types, GenerateCode);
+    }
+
+    private void GenerateCode(SourceProductionContext context, ImmutableArray<ITypeSymbol?> symbols)
+    {
+        if (symbols.IsDefaultOrEmpty)
+            return;
+
+        foreach (var type in symbols)
+        {
+            var builderClass = CreateBuilderClass(type);
+
+            if (builderClass != null)
+            {
+                context.AddSource($"{type?.ContainingNamespace}{type?.Name}.g.cs", builderClass);
+            }
+        }
+    }
+
+    private string? CreateBuilderClass(ITypeSymbol? type)
+    {
+        if (type == null)
+            return null;
+
+        var @namespace = type.ContainingNamespace.ToString();
+        var @class = type.Name;
+        var builderClassName = $"{type.Name}Builder";
+        var methods = "";
+        
+        var propertiesMember = MemberHandler.GetMemberPropertiesWithType(type);
+
+        foreach (var property in propertiesMember)
+        {
+            var methodName = property.Key;
+            methodName = methodName.Remove(1, methodName.Length - 1).ToUpper() +
+                         property.Key.Remove(0, 1);
+
+            var method =
+                $@"
+                    public *builder-class-name* *method-name*(*parameter*)
+                    {{
+                        *lower-class-name*.*property* = *lower-property*;
+                        return this;
+                    }}
+                ";
+
+            method = method
+                .Replace("*builder-class-name*", builderClassName)
+                .Replace("*method-name*", $"Set{methodName}")
+                .Replace("*parameter*", $"{property.Value} {property.Key.ToLower()}")
+                .Replace("*lower-class-name*", @class.ToLower())
+                .Replace("*property*", property.Key)
+                .Replace("*lower-property*", property.Key.ToLower());
+
+            methods += method;
+        }
+
+        var classBuilder = BuilderTemplate.Template
+            .Replace("*namespace*", @namespace)
+            .Replace("*class-name*", @class)
+            .Replace("*builder-class-name*", builderClassName)
+            .Replace("*lower-class-name*", @class.ToLower())
+            .Replace("*methods*", methods);
+        
+        return classBuilder;
+    }
+}
---
+++ SourceGenerate/Templates/Patterns/BuilderTemplate.cs
@@ -0,0 +1,39 @@
+﻿namespace SourceGenerate.Templates.Patterns;
+
+public static class BuilderTemplate
+{
+    public static string Template => GetTemplate();
+    
+    private static string GetTemplate()
+    {
+        return
+            $@"
+                namespace *namespace*
+                {{
+                    partial class *class-name*
+                    {{
+                        public static *builder-class-name* Builder()
+                        {{
+                            return new *builder-class-name*();
+                        }}
+                    }}
+
+                    public class *builder-class-name*
+                    {{
+                        private *class-name* *lower-class-name*;
+                        public *builder-class-name*()
+                        {{
+                            *lower-class-name* = new *class-name*();
+                        }}
+
+                        *methods*
+
+                        public *class-name* Build()
+                        {{
+                            return *lower-class-name*;
+                        }}
+                    }}
+                }}
+            ";
+    }
+}
