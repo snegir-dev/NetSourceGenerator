23739d53c87f6dda4a43b83fd16f6ad5f5e209e6
SnEGiR <steamsnegir@gmail.com>
2022-05-04 23:46:59 +0300

    add generator for all args constructor

---
+++ SourceGenerate/Templates/Constructors/AllArgsConstructorTemplate.cs
@@ -0,0 +1,23 @@
+﻿namespace SourceGenerate.Templates.Constructors;
+
+public static class AllArgsConstructorTemplate
+{
+    public static string Template => GetTemplate();
+
+    private static string GetTemplate()
+    {
+        return
+            @"
+                namespace *namespace*
+                {
+                    partial class *class-name*
+                    {
+                        public *class-name*(*params*)
+                        {
+                            *appropriation-params*
+                        }
+                    }
+                }                
+            ";
+    }
+}
