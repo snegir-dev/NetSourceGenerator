393b0701cdfb1b8c00ecc6f5fd429dcbdd28fe69
SnEGiR <steamsnegir@gmail.com>
2022-05-25 20:45:48 +0300

    create class for tests async methods

---
+++ SourceGenerator.Tests/AsyncMethods/AsyncMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using SourceGenerator.Domain.Attributes;
+using Xunit;
+
+namespace SourceGenerator.Tests.AsyncMethods;
+
+public class AsyncMethodTests
+{
+    [Fact]
+    public void R()
+    {
+        var car = new Car();
+    }
+}
+
+partial class Car
+{
+    [Async]
+    public void Work(int w)
+    {
+    }
+}
