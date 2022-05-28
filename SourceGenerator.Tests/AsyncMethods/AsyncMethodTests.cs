e5526fd685b5cb504723f975d9ece3986c92d6d5
SnEGiR <steamsnegir@gmail.com>
2022-05-28 22:33:47 +0300

    add test for async method return void value

--- SourceGenerator.Tests/AsyncMethods/AsyncMethodTests.cs
+++ SourceGenerator.Tests/AsyncMethods/AsyncMethodTests.cs
@@ -5,17 +5,36 @@ namespace SourceGenerator.Tests.AsyncMethods;
 
 public class AsyncMethodTests
 {
+    private Car _car = new();
+    
     [Fact]
-    public void R()
+    public async void ReturnVoidMethodTest()
     {
-        var car = new Car();
+        // var result = await _car.ReturnVoidMethodAsync("");
+        //
+        // Assert.Equal("Work", result);
+    }
+    
+    [Fact]
+    public async void ReturnStringMethodTest()
+    {
+        var result = await _car.ReturnStringMethodAsync();
+        
+        Assert.Equal("Work", result);
     }
 }
 
-partial class Car
+internal partial class Car
 {
     [Async]
-    public void Work(int w)
+    public void ReturnVoidMethod(out string s)
     {
+        s = "work";
+    }
+    
+    [Async]
+    public string ReturnStringMethod()
+    {
+        return "Work";
     }
 }
