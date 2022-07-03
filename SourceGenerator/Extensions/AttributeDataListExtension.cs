5c5e1eb5dc73b99e77f75ffc20ec144a97cb2761
SnEGiR <steamsnegir@gmail.com>
2022-07-03 15:30:11 +0300

    add AnyInnerAttribute meathod extension

--- SourceGenerator/Extensions/AttributeDataListExtension.cs
+++ SourceGenerator/Extensions/AttributeDataListExtension.cs
@@ -4,11 +4,17 @@ namespace SourceGenerator.Extensions;
 
 public static class AttributeDataListExtension
 {
-    public static bool AnyAttribute(this IEnumerable<AttributeData> attributesData, string attribute)
+    public static bool AnyInnerAttribute(this IEnumerable<AttributeData> attributesData, string attribute)
     {
         return attributesData.Select(a => a.AttributeClass?.GetAttributes())
             .Select(i => i!.Value
                 .Any(a => a.AttributeClass?.Name == attribute))
             .Any(b => b);
+    }
+
+    public static bool AnyAttribute(this IEnumerable<AttributeData> attributesData, string attribute)
+    {
+        return attributesData
+            .Any(a => a.AttributeClass?.Name == attribute);
     }
 }
