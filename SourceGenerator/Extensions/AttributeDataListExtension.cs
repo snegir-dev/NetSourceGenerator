78d27570912c65ed12b596629cd5f1ebd65b3990
SnEGiR <steamsnegir@gmail.com>
2022-07-03 15:21:31 +0300

    add AnyAttribute meathod extension

---
+++ SourceGenerator/Extensions/AttributeDataListExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGenerator.Extensions;
+
+public static class AttributeDataListExtension
+{
+    public static bool AnyAttribute(this IEnumerable<AttributeData> attributesData, string attribute)
+    {
+        return attributesData.Select(a => a.AttributeClass?.GetAttributes())
+            .Select(i => i!.Value
+                .Any(a => a.AttributeClass?.Name == attribute))
+            .Any(b => b);
+    }
+}
